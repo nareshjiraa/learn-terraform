8277bd6791483cc606d3cd354ee2b1b0ea930ff1
nareshjiraa <130171324+nareshjiraa@users.noreply.github.com>
2023-07-01 18:43:10 +0530

    command substitution

--- servers.tf
+++ servers.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" "Payment" {
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+  owners           = ["self"]
+
+  resource "aws_instance" "Payment" {
   ami           = "ami-0cdb8266fcd5d3d63"
   instance_type = "t2.micro"
 
