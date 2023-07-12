64d5af8edef2797220cb2da8c5d5bf48894d617d
nareshjiraa <130171324+nareshjiraa@users.noreply.github.com>
2023-07-12 10:20:43 +0530

    command substitution

--- variable.tf
+++ variable.tf
@@ -1,11 +1,26 @@
 #Declare the Variable
 ## Shell scripting equvivalalnt sample_string ="Hello World"
-variable sample_string {
+variable "sample_string" {
   default = "Hello World"
 }
-## Shell scripting equvivalalnt echo $sample_string
+
+variable "sample_number" {
+  default = 100
+}
+
+variable "sample_boolean" {
+  default = true
+}
+#Booleans are true or false
+#Booleans & numbers don't need double quotes. Terraform doesnt support single quotes
+#strings need double quotes
+
+
 
 #Access the variable
+## Shell scripting equvivalalnt echo $sample_string
+
+
 output "sample_string" {
   value = "var.sample_string"
 }
