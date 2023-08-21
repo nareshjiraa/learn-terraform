7d0c0e6c3f93a189d0128eb827244b536fca0c05
nareshjiraa <130171324+nareshjiraa@users.noreply.github.com>
2023-08-21 16:01:49 +0530

    command substitution

--- create instances.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_instance" "web" {
-  ami           = "ami-0cdb8266fcd5d3d63"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
--- variable.tf
+++ variable.tf
@@ -1,36 +1,7 @@
-#Declare the Variable
-## Shell scripting equvivalalnt sample_string ="Hello World"
-variable "sample_string" {
+variable "sample1" {
   default = "Hello World"
 }
 
-variable "sample_number" {
-  default = 100
-}
-
-variable "sample_boolean" {
-  default = true
-}
-#Booleans are true or false
-#Booleans & numbers don't need double quotes. Terraform doesnt support single quotes
-#strings need double quotes
-
-variable "env" {}
-
-
-#Access the variable
-## Shell scripting equvivalalnt echo $sample_string
-
-
-output "sample_string" {
-  value = "var.sample_string"
-}
-
-
-output "sample_string1" {
-  value = "value of sample_string1 = ${var.sample_string}"
-}
-
-output "env" {
-  value = var.env
+output "sample1" {
+  value = "var.sample1"
 }
